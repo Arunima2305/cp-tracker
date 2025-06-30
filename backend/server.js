0e556d5085b61ab5e2d6cd23d8568d4d495b5c24
Arunima2305 <2023csb1101@iitrpr.ac.in>
2025-06-30 22:57:28 +0530

    changesss

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,9 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/questions", questionRoutes);
 app.use("/api/parse-question", parseQuestionRoutes);
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
